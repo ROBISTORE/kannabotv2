7f163380e9d5c35c40ea23cf9a451c387fd8738f
ROBISTORE <107118851+ROBISTORE@users.noreply.github.com>
2022-06-15 12:06:39 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,12 +20,12 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-*ɴᴀᴍᴇ:* ⓊⒸⒽⒾⒽⒶ ⓃⒶⓇⓊⓉⓄ
-*ᴀɢᴇ:* 20ᵗʰ
-*sᴛᴀᴛᴜs:* single 
+*ɴᴀᴍᴇ:* ROBI STORE
+*ᴀɢᴇ:* 15ᵗʰ
+*sᴛᴀᴛᴜs: single 
 
-*ʙɪʀᴛʜᴅᴀʏ:* 21 Des 2002
-*ᴀᴅᴅʀᴇss:* Indonesia, Jawa Barat, Bandung
+*ʙɪʀᴛʜᴅᴀʏ:* 28 Juni 2008
+*ᴀᴅᴅʀᴇss:* Indonesia, Palembang, Prabumulih
 
 *- - sᴋɪʟʟs: - -* 
 > JavaScript [89.7%]
